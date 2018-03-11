a8902f101d60c91d0ede23eb2a29ecfa530a3d3a
emin bayrak <eminb@outlook.com>
2018-03-10 22:53:53 -0500

    test

--- ParPorApp/ParPorApp/ViewModels/AddEventViewModel.cs
+++ ParPorApp/ParPorApp/ViewModels/AddEventViewModel.cs
@@ -17,11 +17,11 @@ namespace ParPorApp.ViewModels
     {
 		ApiServices _apiServices = new ApiServices();
 	    public string Description { get; set; }
-	    public string EndDateTime { get; set; }
+	    public DateTime EndDateTime { get; set; }
 	    public string GroupId { get; set; }
 	    public string Id { get; set; }
 	    public string LocationId { get; set; }
-	    public string StartDateTime { get; set; }
+	    public DateTime StartDateTime { get; set; }
 	    public string Name { get; set; }
 
 		public ICommand AddEventCommand
